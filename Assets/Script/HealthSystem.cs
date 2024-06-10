7048ee9637780310b9c378c3acc26a60c9d886e9
RyotaTsushima <ryota.00.08.08@gmail.com>
2024-06-10 19:27:43 +0900

    a

--- Assets/Script/HealthSystem.cs
+++ Assets/Script/HealthSystem.cs
@@ -8,19 +8,22 @@ public class HealthSystem : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] float m_maxHealth = default;   //Healthは秒数。何秒耐えられるかを表す
     [SerializeField] float m_scale = default;   //半径はスケールの1/2
-    public float m_health;
+    [SerializeField] float m_itemTime = default;
+    public static float m_health;
     float m_per;
     Vector2 _scale1;
+    int _itemnum = 0;
+    int _itemnum2 = 0;
     void Start()
     {
         _scale1 = transform.localScale* m_scale;
         transform.localScale = _scale1;
-        
+        _itemnum = 0;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Rain")
+        if (collision.tag == "Rain"&&_itemnum!=1)
         {
             Debug.Log("It is raining,");
             m_health -= Time.deltaTime;
@@ -29,10 +32,24 @@ public class HealthSystem : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Puddle")
+        if(collision.tag == "Puddle"&&_itemnum!=1)
         {
             Debug.Log("I fall down to puddle.");
             m_health = 0f;
+        }
+
+        if(collision.tag == "Umbrella")
+        {
+            Debug.Log("傘を手に入れた");
+            _itemnum = 1;
+            StartCoroutine(ItemReset());
+        }
+
+        if (collision.tag == "Boat")
+        {
+            Debug.Log("船に乗った");
+            _itemnum2 = 1;
+            StartCoroutine(ItemReset());
         }
     }
 
@@ -45,5 +62,11 @@ public class HealthSystem : MonoBehaviour
         }
     }
 
-    
+    IEnumerator ItemReset()
+    {
+        yield return new WaitForSeconds(m_itemTime);
+        _itemnum = 0;
+        _itemnum2=0;
+        Debug.Log("アイテムが壊れた");
+    }
 }
